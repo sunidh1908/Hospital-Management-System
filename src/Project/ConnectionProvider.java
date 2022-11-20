28713e799c8803a3381b4be90106fc07061fd263
sunidh1908 <sunidh.kumar@gmail.com>
2022-11-20 13:59:34 +0530

    first commit

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,30 @@
+package Project;
+import java.sql.*;
+/*
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author HP
+ */
+public class ConnectionProvider {
+ 
+    public static Connection getCon(){
+        
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","123456");
+            return con;
+        }
+            
+        catch(Exception e)
+        {
+           return null; 
+        }
+    }
+}
